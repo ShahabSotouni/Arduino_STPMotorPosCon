f025f993aa32a8e650ea780017791f06dae5037b
Shahab Sotouni <Shahab Sotouni@DESKTOP-1DA8TVV.aero.edu>
2018-04-08 09:26:52 +0430

    File Name And Directory

--- ENCODER/ENCODER.ino
+++ ENCODER/ENCODER.ino
@@ -1,9 +1,32 @@
+#include <Wire.h>
 #define encoder0PinA 2
 #define encoder0PinB 3
 volatile  int encoder0Pos = 0;
 unsigned int tmp = 0;
 unsigned int Aold = 0;
 unsigned int Bnew = 0;
+double deg;
+int degint=12345;
+
+
+byte flag = 0;
+
+//Blink LED
+void blinkled()
+{
+  if(flag)
+  {
+    flag = 0;
+    digitalWrite(13, LOW);
+  }
+  else
+  {
+    flag = 1;
+    digitalWrite(13, HIGH);
+  }
+}
+
+
 void setup() {
   pinMode(encoder0PinA, INPUT);
   pinMode(encoder0PinB, INPUT);
@@ -13,11 +36,15 @@ void setup() {
   attachInterrupt(1, doEncoderB, CHANGE);
   // set up the Serial Connection
   Serial.begin (115200);
+  Wire.begin(0x38);             // i2c in slave mode address is 0x38
+  Wire.onRequest(requestEvent);
 }
+
 void loop() {
   //Check each changes in position
   if (tmp != encoder0Pos) {
-    double deg=encoder0Pos*360.0/1000.0/4.0/20.0;
+    deg=encoder0Pos*360.0/1000.0/4.0/20.0;
+    degint=100*deg;
     Serial.println(deg);
     tmp = encoder0Pos;
   }
@@ -28,8 +55,24 @@ void doEncoderA() {
   Bnew^Aold ? encoder0Pos++ : encoder0Pos--;
   Aold = digitalRead(encoder0PinA);
 }
+
 // Interrupt on B changing state
 void doEncoderB() {
   Bnew = digitalRead(encoder0PinB);
   Bnew^Aold ? encoder0Pos++ : encoder0Pos--;
+}
+
+void requestEvent()
+{
+  byte sendhi = 0, sendli = 0;
+  byte sendbyte[3];
+  int16_t tempreading_deg;
+  tempreading_deg=degint;
+  sendhi=tempreading_deg>>8;
+  sendli=tempreading_deg&0xff;
+  sendbyte[0]=sendhi;
+  sendbyte[1]=sendli;
+  sendbyte[2]=(sendhi+sendli)&0xff;
+  Wire.write(sendbyte,3); 
+  blinkled();
 }
